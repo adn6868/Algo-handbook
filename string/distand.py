2475f3ea26a2a4ee22c2a5e54e90371f8152a934
Anh Nguyen_MacbookPro <anhdnguyenn@gmail.com>
2019-02-26 15:12:29 -0600

    string calculation

---
+++ string/distand.py
@@ -0,0 +1,26 @@
+'''
+calculate the distance between a string to another string
+'''
+
+def distance(first,second):
+    if len(first) < len(second):
+        first, second = second, first
+    if len(second) == 0:
+        return len(first)
+    pre_row = range(len(second) + 1)
+    for i , c1 in enumerate(first):
+        cur_row = [i + 1]
+        for j, c2 in enumerate(second):
+            insertions = pre_row[j+1] + 1
+            deletions = cur_row[j] + 1
+            subtitute = pre_row[j] + (c1 != c2)
+            cur_row.append(min(insertions, deletions, subtitute))
+        pre_row = cur_row
+    return pre_row[-1]
+
+print(distance('a','a'))
+print(distance('a',''))
+print(distance('','b'))
+print(distance('abc','cdb'))
+print(distance('cde','abcdefgh'))
+# print(distance('a','b'))
